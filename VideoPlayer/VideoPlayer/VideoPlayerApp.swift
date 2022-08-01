4ebd72eb590c948f8d8f016c284f91f64949002c
oguzhanjanberk <oguzhan.ozzie@gmail.com>
2022-08-01 18:50:12 +0100

    Add files via upload

---
+++ VideoPlayer/VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Oguzhan Janberk on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
